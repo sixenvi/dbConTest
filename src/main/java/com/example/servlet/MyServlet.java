e288fac279a8a4b43b82cd236ef9053a1b231b4d
Leroy Elendt <sixenvi@gmail.com>
2019-05-29 16:12:20 +0200

    Added oracle driver loading

--- src/main/java/com/example/servlet/MyServlet.java
+++ src/main/java/com/example/servlet/MyServlet.java
@@ -3,11 +3,15 @@ package com.example.servlet;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.inject.Inject;
 import javax.servlet.*;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+
+import java.sql.*;
+
 
 public class MyServlet extends HttpServlet{
     private static final Logger log = LoggerFactory.getLogger(MyServlet.class);
@@ -15,9 +19,23 @@ public class MyServlet extends HttpServlet{
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException
     {
+        getDriver();
+
         resp.setContentType("text/html");
         resp.getWriter().write("<html><body>Hello World</body></html>");
     }
-    
+
+
+    protected void getDriver() {
+        try {
+            Class.forName("oracle.jdbc.OracleDriver");
+        } catch (ClassNotFoundException e) {
+            log.error("ClassNotFoundException!", e);
+            e.printStackTrace();
+        }
+
+
+    }
+
 
 }
